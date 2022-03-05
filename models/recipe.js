67ba4921dfcbdde4ad82a670bfe18fec50dd1b71
Justin Vongdara <vongdara16@gmail.com>
2022-03-05 14:31:47 -0500

    add recipe model

---
+++ models/recipe.js
@@ -0,0 +1,18 @@
+
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const recipeSchema = new Schema({
+  name: {type: String, required: true},
+  link: String,
+  instructions: String,
+  quantity: Number,
+  author: {type: Schema.Types.ObjectId, ref: "Profile"}
+})
+
+const Recipe = mongoose.model('Taco', tacoSchema)
+
+export {
+  Recipe
+}
